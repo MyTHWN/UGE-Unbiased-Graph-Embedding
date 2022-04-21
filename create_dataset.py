ea03fb3b07aa3277c1988285b7d1346a005a6703
Jinghui Chen <jzc5917@I4-GL-JZC5917-1.AD.PSU.EDU>
2022-04-21 17:00:06 -0400

    automatically download movielens via url and process datasets when load them

--- create_dataset.py
+++ create_dataset.py
@@ -6,15 +6,17 @@ We store three components for each dataset
 -- edge.csv: store the edges
 """
 
+import torch
 import dgl
 from dgl.data import DGLDataset
-import torch
 from dgl import backend as F
 import os
 import urllib.request
+import zipfile
 import pandas as pd
 import numpy as np
 import networkx as nx
+import os.path
 
 class MyDataset(DGLDataset):
     def __init__(self, data_name):
@@ -22,15 +24,37 @@ class MyDataset(DGLDataset):
         super().__init__(name='customized_dataset')
 
     def process(self):
-        # Load the data as DataFrame
-        node_features = pd.read_csv('./mydata/{}_node_feature.csv'.format(self.data_name.split('_')[0]))
-        node_labels = pd.read_csv('./mydata/{}_node_label.csv'.format(self.data_name.split('_')[0]))
-        edges = pd.read_csv('./mydata/{}_edge.csv'.format(self.data_name))
+        raw_folder = './raw_data'
+        processed_folder = './processed_data'
         
-        node_attributes = None
-        if os.path.isfile('./mydata/{}_node_attribute.csv'.format(self.data_name.split('_')[0])):
-            node_attributes = pd.read_csv('./mydata/{}_node_attribute.csv'.format(self.data_name.split('_')[0]))
-
+        os.makedirs(raw_folder, exist_ok=True)
+        os.makedirs(processed_folder, exist_ok=True)
+        
+        edge_file = '{}/{}_edge.csv'.format(processed_folder, self.data_name) 
+        node_feat_file = '{}/{}_node_feature.csv'.format(processed_folder, self.data_name.split('_')[0])
+        node_label_file = '{}/{}_node_label.csv'.format(processed_folder, self.data_name.split('_')[0])
+        node_attribute_file = '{}/{}_node_attribute.csv'.format(processed_folder, self.data_name.split('_')[0])  # sensitive node attributes predefined to debias
+        
+        ### download raw data and process into unified csv format ###
+        
+        if not os.path.isfile(node_feat_file):
+            if self.data_name.split('_')[0] == 'movielens':
+                process_raw_movielens(raw_folder, processed_folder)
+            elif self.data_name.split('_')[0].startswith('pokec'):
+                process_raw_pokec(raw_folder, processed_folder, self.data_name.split('_')[0])
+            else:
+                raise FileNotFoundError('The dataset {} is not supported'.format(self.data_name.split('_')[0]))
+        
+        ### create dgl graph from customized data ###
+        
+        print ('Creating DGL graph...')
+        
+        # Load the data as DataFrame
+        edges = pd.read_csv(edge_file, engine='python')
+        node_features = pd.read_csv(node_feat_file, engine='python')
+        node_labels = pd.read_csv(node_label_file, engine='python')
+        node_attributes = pd.read_csv(node_attribute_file, engine='python')
+        
         c = node_labels['Label'].astype('category')
         classes = dict(enumerate(c.cat.categories))
         self.num_classes = len(classes)
@@ -43,21 +67,15 @@ class MyDataset(DGLDataset):
         edges_dst = torch.from_numpy(edges['Dst'].to_numpy())
 
         # construct DGL graph
-        # !!! note to turn a directed graph into a undirected one
-        # otherwise the graph embedding performance might be compromised
         g = dgl.graph((edges_src, edges_dst), num_nodes=node_features.shape[0])
         g.ndata['feat'] = node_features
         g.ndata['label'] = node_labels
         g.edata['weight'] = edge_features
         
-        nx_g = dgl.to_networkx(g)
-        print(nx.classes.function.density(nx_g))
-        print(nx_g.number_of_nodes())
-        print(nx_g.number_of_edges())
         
-        if node_attributes is not None:
-            for l in list(node_attributes):
-                g.ndata[l] = torch.from_numpy(node_attributes[l].to_numpy()).long()
+        # add sensitive attribute information to graph
+        for l in list(node_attributes):
+            g.ndata[l] = torch.from_numpy(node_attributes[l].to_numpy()).long()
                 
         # rewrite the to_bidirected function to support edge weights on bidirected graph (aggregated)
         # self.graph = dgl.to_bidirected(g)
@@ -67,10 +85,11 @@ class MyDataset(DGLDataset):
         # zero in-degree nodes will lead to invalid output value
         # a common practice to avoid this is to add a self-loop
         self.graph = dgl.add_self_loop(g)
-        # self.graph = g
 
-        # If your dataset is a node classification dataset, you will need to assign
+        # For node classification task, you will need to assign
         # masks indicating whether a node belongs to training, validation, and test set.
+        # ! We currently only target on link prediction task
+        # ! this is a placeholder for node classification task
         n_nodes = node_features.shape[0]
         n_train = int(n_nodes * 0.6)
         n_val = int(n_nodes * 0.2)
@@ -88,13 +107,13 @@ class MyDataset(DGLDataset):
         print('  NumNodes: {}'.format(self.graph.number_of_nodes()))
         print('  NumEdges: {}'.format(self.graph.number_of_edges()))
         print('  NumFeats: {}'.format(self.graph.ndata['feat'].shape[1]))
-        print('  NumClasses: {}'.format(self.num_classes))
-        print('  NumTrainingSamples: {}'.format(
-            F.nonzero_1d(self.graph.ndata['train_mask']).shape[0]))
-        print('  NumValidationSamples: {}'.format(
-            F.nonzero_1d(self.graph.ndata['val_mask']).shape[0]))
-        print('  NumTestSamples: {}'.format(
-            F.nonzero_1d(self.graph.ndata['test_mask']).shape[0]))
+        # print('  NumClasses: {}'.format(self.num_classes))
+        # print('  NumTrainingSamples: {}'.format(
+        #     F.nonzero_1d(self.graph.ndata['train_mask']).shape[0]))
+        # print('  NumValidationSamples: {}'.format(
+        #     F.nonzero_1d(self.graph.ndata['val_mask']).shape[0]))
+        # print('  NumTestSamples: {}'.format(
+        #     F.nonzero_1d(self.graph.ndata['test_mask']).shape[0]))
 
     def __getitem__(self, i):
         return self.graph
@@ -103,66 +122,59 @@ class MyDataset(DGLDataset):
         return 1
 
 
-def process_raw_karate():
-    os.makedirs('./tmp', exist_ok=True)
-    os.makedirs('./mydata', exist_ok=True)
-
-    edge_tmp_file = './tmp/interactions.csv'
-    node_tmp_file = './tmp/members.csv'
-
-    urllib.request.urlretrieve(
-        'https://data.dgl.ai/tutorial/dataset/members.csv', node_tmp_file)
-    urllib.request.urlretrieve(
-        'https://data.dgl.ai/tutorial/dataset/interactions.csv', edge_tmp_file)
-
-    edges = pd.read_csv(edge_tmp_file)
-    nodes = pd.read_csv(node_tmp_file)
-    nodes['Label'] = nodes['Club'].astype('category').cat.codes
-
-    node_feature = pd.DataFrame(nodes['Age'])
-    node_label = nodes[['Label', 'Club']]
+def process_raw_movielens(raw_folder, processed_folder):
     
-    node_feature.to_csv('./mydata/karate_node_feature.csv', sep=',')
-    node_label.to_csv('./mydata/karate_node_label.csv', sep=',')
-    edges.to_csv('./mydata/karate_edge.csv', sep=',')
-
-
-# TODO: Define the other datasets as you like
-def process_raw_movielens():
-    genre_ls = ["Action",  "Adventure",  "Animation",  "Children's",  "Comedy",
+    ### download dataset ###
+    
+    print ('Downloading movielens data...')
+    
+    edge_file = '{}/ml-1m/ratings.dat'.format(raw_folder)
+    users_file = '{}/ml-1m/users.dat'.format(raw_folder)
+    items_file = '{}/ml-1m/movies.dat'.format(raw_folder)
+    
+    filehandle, _ = urllib.request.urlretrieve("https://files.grouplens.org/datasets/movielens/ml-1m.zip")
+    zip_file_object = zipfile.ZipFile(filehandle, 'r')
+    zip_file_object.extractall(raw_folder)
+    
+    edges = pd.read_csv(edge_file, sep='::', 
+                    names=['Src', 'Dst', 'Weight', 'time'], engine='python')
+    user_nodes = pd.read_csv(users_file, sep='::', 
+                    names=['user', 'gender', 'age', 'occupation', 'zip'], engine='python')
+    item_nodes = pd.read_csv(items_file, sep='::', 
+                    names=['movie', 'title', 'genre'], encoding='latin-1', engine='python')
+    
+    print ('Downloaded to {}'.format(raw_folder))
+    
+    ### process dataset ###
+    
+    print ('Converting to csv format...')
+    
+    feature_ls = ["Action",  "Adventure",  "Animation",  "Children's",  "Comedy",
     "Crime",  "Documentary",  "Drama",  "Fantasy",  "Film-Noir",
     "Horror",  "Musical",  "Mystery",  "Romance",  "Sci-Fi",
     "Thriller",  "War",  "Western"]
-
-    os.makedirs('./mydata', exist_ok=True)
-
-    edge_file = '../ml-1m/ratings.dat'
-    users_file = '../ml-1m/users.dat'
-    items_file = '../ml-1m/movies.dat'
-
-    edges = pd.read_csv(edge_file, sep='::', 
-                    names=['Src', 'Dst', 'Weight', 'time'])
-    user_nodes = pd.read_csv(users_file, sep='::', 
-                    names=['user', 'gender', 'age', 'occupation', 'zip'])
-    item_nodes = pd.read_csv(items_file, sep='::', 
-                    names=['movie', 'title', 'genre'], encoding='latin-1')
-
+    
+    sensitive_attributes_predefined = ['gender', 'occupation', 'age']
+    
+    user_num = 6040
+    item_num = 3952
+    
     user_nodes['label'] = 1
     item_nodes['label'] = 0
 
     edges['Src'] = edges['Src'].astype(int) - 1
-    edges['Dst'] = edges['Dst'].astype(int) + 6040 - 1
+    edges['Dst'] = edges['Dst'].astype(int) + user_num - 1
     user_nodes['user'] = user_nodes['user'].astype(int) - 1 
-    item_nodes['movie'] = item_nodes['movie'].astype(int) + 6040 - 1 
+    item_nodes['movie'] = item_nodes['movie'].astype(int) + user_num - 1 
 
     weighted_edges = edges[['Src', 'Dst', 'Weight']]
     
-    user_features = {genre:np.zeros(6040) for genre in genre_ls}
-    item_features = {genre:np.zeros(3952) for genre in genre_ls}
+    user_features = {genre:np.zeros(user_num) for genre in feature_ls}
+    item_features = {genre:np.zeros(item_num) for genre in feature_ls}
 
     for row in item_nodes.iterrows():
         idx = row[0]
-        item = int(row[1]['movie']) - 6040
+        item = int(row[1]['movie']) - user_num
         genres = row[1]['genre']
         genres = genres.strip().split('|')
 
@@ -172,9 +184,9 @@ def process_raw_movielens():
     for row in edges.iterrows(): 
         edge = row[1]
         user = int(edge['Src'])
-        item = int(edge['Dst']) - 6040
+        item = int(edge['Dst']) - user_num
 
-        for genre in genre_ls:
+        for genre in feature_ls:
             if item_features[genre][item] == 1:
                 user_features[genre][user] = 1
 
@@ -183,29 +195,42 @@ def process_raw_movielens():
 
     node_features = pd.concat([user_features, item_features], ignore_index=True)
     node_labels = pd.DataFrame(
-            {'Label':np.concatenate((np.ones(6040), np.zeros(3952)))})
+            {'Label':np.concatenate((np.ones(user_num), np.zeros(item_num)))})
     
-    user_attributes = user_nodes.filter(['gender', 'occupation', 'age']).replace(['F', 'M'], [0, 1])
-    item_attributes = pd.DataFrame(-1, index=np.arange(len(node_labels)-len(user_attributes)), columns=['gender', 'occupation', 'age'])
+    user_attributes = user_nodes.filter(sensitive_attributes_predefined).replace(['F', 'M'], [0, 1])
+    item_attributes = pd.DataFrame(-1, index=np.arange(len(node_labels)-len(user_attributes)), columns=sensitive_attributes_predefined)
     node_attributes = pd.concat([user_attributes, item_attributes], ignore_index=True)
     
-    print(len(user_nodes), len(item_nodes), len(user_features), len(item_features))
+    node_attributes.to_csv('{}/movielens_node_attribute.csv'.format(processed_folder), sep=',', index=False)
+    node_features.to_csv('{}/movielens_node_feature.csv'.format(processed_folder), sep=',', index=False)
+    node_labels.to_csv('{}/movielens_node_label.csv'.format(processed_folder), sep=',', index=False)
+    weighted_edges.to_csv('{}/movielens_edge.csv'.format(processed_folder), sep=',', index=False)
     
-    node_attributes.to_csv('./mydata/movielens_node_attribute.csv', sep=',', index=False)
-    node_features.to_csv('./mydata/movielens_node_feature.csv', sep=',', index=False)
-    node_labels.to_csv('./mydata/movielens_node_label.csv', sep=',', index=False)
-    weighted_edges.to_csv('./mydata/movielens_edge.csv', sep=',', index=False)
+    print ('statistics: #user={}, #item={}, #user_feature={}, #item_feature={}'.format(len(user_nodes), len(item_nodes), len(user_features), len(item_features)))
+    print ('Processed data to {}'.format(processed_folder))
 
 
-def process_raw_pokec():
+def process_raw_pokec(raw_folder, processed_folder, data_name):
+    
+    print ('Converting to csv format...' )
+    
+    if data_name == 'pokec-z':
+        edge_file = '{}/pokec/region_job_relationship.txt'.format(raw_folder)
+        node_file = '{}/pokec/region_job.csv'.format(raw_folder)
+        
+    elif data_name == 'pokec-n':
+        edge_file = '{}/pokec/region_job_2_relationship.txt'.format(raw_folder)
+        node_file = '{}/pokec/region_job_2.csv'.format(raw_folder)
 
-    os.makedirs('./mydata', exist_ok=True)
-
-    edge_file = '../pokec/region_job_2_relationship.txt'
-    node_file = '../pokec/region_job_2.csv'
-
-    edges = pd.read_csv(edge_file, sep='\t', names=['Src', 'Dst'])
-    nodes = pd.read_csv(node_file, sep=',', header=0)
+    edges = pd.read_csv(edge_file, sep='\t', names=['Src', 'Dst'], engine='python')
+    nodes = pd.read_csv(node_file, sep=',', header=0, engine='python')
+    
+    print ('-- raw data loaded')
+    
+    feature_ls = ['Label', 'user_id', 'public', 
+                  'completion_percentage', 'gender', 'region', 'AGE']
+    
+    sensitive_attributes_predefined = ['gender', 'region', 'AGE']
 
     node_ids = list(nodes['user_id'])
     new_ids = list(range(len(node_ids)))
@@ -218,20 +243,18 @@ def process_raw_pokec():
     edges['Src'].replace(id_map, inplace=True)
     edges['Dst'].replace(id_map, inplace=True)
 
-    node_attributes = nodes.filter(['gender', 'region', 'AGE'])
-    node_features = nodes.drop(columns=['Label', 'user_id', 'public', 
-        'completion_percentage', 'gender', 'region', 'AGE'])
-
-    node_attributes.to_csv('./mydata/pokec2_node_attribute.csv', sep=',', index=False)
-    node_features.to_csv('./mydata/pokec2_node_feature.csv', sep=',', index=False)
-    node_labels.to_csv('./mydata/pokec2_node_label.csv', sep=',', index=False)
-    edges.to_csv('./mydata/pokec2_edge.csv', sep=',', index=False)
+    node_attributes = nodes.filter(sensitive_attributes_predefined)
+    node_features = nodes.drop(columns=feature_ls)
     
+    print ('-- feature and attribute filtered')
 
-# First process data into the unified csv format
-# process_raw_karate()
-process_raw_movielens()
-# process_raw_pokec()
+    node_attributes.to_csv('{}/{}_node_attribute.csv'.format(processed_folder, data_name), sep=',', index=False)
+    node_features.to_csv('{}/{}_node_feature.csv'.format(processed_folder, data_name), sep=',', index=False)
+    node_labels.to_csv('{}/{}_node_label.csv'.format(processed_folder, data_name), sep=',', index=False)
+    edges.to_csv('{}/{}_edge.csv'.format(processed_folder, data_name), sep=',', index=False)
+    
+    print ('Processed data to {}'.format(processed_folder))
+
 
 
 
